07fa2b96b479101412f3f2f7931ee381e86262d9
kernel_loophole <p190119@nu.edu.pk>
2023-02-14 01:56:24 +0500

    ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -3,10 +3,10 @@ fn main()
     // let s = vec!["udon".to_string(), "ramen".to_string(),
     // "soba".to_string()];
     // println!("{}",s)
-    // drop_out_string();
-    // no_drop_out_ex();
-    // moves_and_control();
-    // another_example_of_moves();
+    drop_out_string();
+    no_drop_out_ex();
+    moves_and_control();
+    another_example_of_moves();
     moving_example_of_int();
     }
 
@@ -67,6 +67,8 @@ fn another_example_of_moves()
         println!("{}",s)
     }
 }
+//integers in Rust 
+//
 fn moving_example_of_int()
 {
     let num1:i32=32;
