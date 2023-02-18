1e318724f40627ea98cde68df787f61f3c35bcb0
kernel_loophole <p190119@nu.edu.pk>
2023-02-19 01:16:57 +0500

    update

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -3,11 +3,14 @@ fn main()
     // let s = vec!["udon".to_string(), "ramen".to_string(),
     // "soba".to_string()];
     // println!("{}",s)
-    drop_out_string();
-    no_drop_out_ex();
-    moves_and_control();
-    another_example_of_moves();
-    moving_example_of_int();
+    // drop_out_string();
+    // no_drop_out_ex();
+    // moves_and_control();
+    // another_example_of_moves();
+    // moving_example_of_int();
+    // shadowing();
+    // mut_and_imut();
+    freezing_val();
     }
 
 fn drop_out_string()
@@ -75,4 +78,47 @@ fn moving_example_of_int()
     let num2=num1;
     println!(" value of num1 {}\n",num1);
     println!("value of num2 {}\n",num2);
+}
+//shadowing the value 
+fn shadowing()
+{
+    let x=12;
+    let x=12.6;
+    println!("the value of x ===>{}",x);
+    // mut the value 
+    let mut y=20;
+    let y=25;
+   
+    // defining the scope of var
+    //the lifetime of y is inside the bracket only 
+    {
+        let y=30;
+        println!("the value of y inside the block is {}",y);
+    }
+    println!("the value of y is {}",y);
+
+
+}
+fn mut_and_imut()
+{
+    let some_value=13;
+    
+    // this can not be done as varibles are imutable by default
+            //   some_value+=1;
+    println!("the value of vari{}",some_value);
+    //update the value of some_value varibles 
+    let some_value=20;
+    println!("after assing new value{}",some_value);
+
+}
+fn freezing_val()
+{
+    let mut _mut_val=20;
+    {
+        let _mut_val=40;
+        //cannnot assign value to imutable variable
+        //_mut_val=10;
+        println!("{}",_mut_val);
+    }
+
 }
