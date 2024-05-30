61f93eb193dd5c3d6138b3b6be8f90afdac7e6ee
kernel_loophole <p190119@nu.edu.pk>
2024-05-30 06:32:48 -0400

    anagram group function updated

--- src/practice.rs
+++ src/practice.rs
@@ -1,138 +1,87 @@
 extern crate regex;
-use std::thread;
-use std::sync::mpsc;
-use std::time::Duration;
+
+use std::collections::HashMap;
 use std::env::var;
 use console::style;
 
 
 use regex::Regex;
-// fn main() {
-//     // let vec1=vec![1,13,5,6,6];
-//     // let iter_test:Vec<_>=vec1.iter().map(|x| x+2).collect();
-//     // println!("{:?}",iter_test);
-//     // let mut str_test=String::from("test str");
-//     // let str_test_one:&str="test str";
-//     // println!("{:?}",str_test);
-//     // println!("{:?}",str_test_one);
-//     // let mut mut_x=&str_test;
-//     // println!("{:?}",mut_x);
-//     // println!("{:?}",str_test);
-//     // let mut test_final_str=String::from("helo str");
-//     // let mut final_str=this_function(&test_final_str);
-//     // final_str.retain(|c| c!='h');
-//     // println!("{:?}",final_str);
-//     // println!("{:?}",test_final_str);
-//   //   let test_str:String=String::from("[{}()]({}{})");
-//   // if   valid_par(&test_str){
-//   //     println!("true")
-//   // }
-//   //   else {
-//   //       println!("flase");
-//   //   }
-//
-//     // let palin=String::from("A man, a plan, a canal: Panama");
-//     // if palindrome(&palin)
-//     // {
-//     //     println!("true")
-//     // }
-//     // else {
-//     //     println!("flase");
-//     // }
-//
-//     // ==================haystack problem=============
-//     // let haytsack=String::from("sadsoiamsad");
-//     // let needle=String::from("sad");
-//     // let res=str_str(&haytsack,&needle);
-//     // println!("{:?}",res);
-//     // println!("This is {} neat", style("quite").blue());
-//     // println!("This is {} neat", style("testing color").red());
-//     // let nums=vec![1,3,4,4,3,10];
-//     // let target=13;
-//     // let rs=two_sum(nums,target);
-//     // println!("{:?}",rs);
-//     // let version1="1.20";
-//     // let version=String::from("1.30");
-//     // let version2=String::from("1.30");
-//     // let ver_call=compare_version(&version,&version2);
-//     // println!("{}",ver_call);
-//     // =======================word_dic code============
-//     // let mut word_dic=vec!["apple","pen","apple","dog"];
-//     // let word=String::from("applepen");
-//     // // word_dic.push("apple");
-//     // // word_dic.push("pen");
-//     // // word_dic.push("apple");
-//     // if word_break(&word,word_dic)
-//     // { println!("true") }
-//     // else { println!("false") }
-//     //==============Reverse a string like hello word======>word hello =======
-//     // let s=String::from("hello world");
-//     // let final_str=reverse_words(s);
-//     // println!("{:?}",final_str);
-//     let array:Vec<i32>=vec![1,23,34,4];
-//     permute(array);
-//
-//
-// }
-//thread main function
 fn main() {
-   //=======simple thread while end when main thread end
-    // thread::spawn(|| {
-    //     for i in 1..10 {
-    //         println!("hi number {} from the spawned thread!",
-    //                  i);
-    //     }
-    // });
-    // for i in 1..5 {
-    //     println!("hi number {} from the main thread!",
-    //              i);
+    // let vec1=vec![1,13,5,6,6];
+    // let iter_test:Vec<_>=vec1.iter().map(|x| x+2).collect();
+    // println!("{:?}",iter_test);
+    // let mut str_test=String::from("test str");
+    // let str_test_one:&str="test str";
+    // println!("{:?}",str_test);
+    // println!("{:?}",str_test_one);
+    // let mut mut_x=&str_test;
+    // println!("{:?}",mut_x);
+    // println!("{:?}",str_test);
+    // let mut test_final_str=String::from("helo str");
+    // let mut final_str=this_function(&test_final_str);
+    // final_str.retain(|c| c!='h');
+    // println!("{:?}",final_str);
+    // println!("{:?}",test_final_str);
+    //   let test_str:String=String::from("[{}()]({}{})");
+    // if   valid_par(&test_str){
+    //     println!("true")
+    // }
+    //   else {
+    //       println!("flase");
+    //   }
+
+    // let palin=String::from("A man, a plan, a canal: Panama");
+    // if palindrome(&palin)
+    // {
+    //     println!("true")
+    // }
+    // else {
+    //     println!("flase");
     // }
 
-   //==============Second thread example which end when span ends ================
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {} from the spawned thread!",
-                     i);
-        }
-    });
-    for i in 1..5 {
-        println!("hi number {} from the main thread!",
-                 i);
-    }
-    let v=vec![1,2,3,4,4];
-    //Force colsure o take  the onwership of v as reference
-    let test_thread=thread::spawn(move || {
-        println!("{:?}",v);
-    });
-
-    handle.join();
-    test_thread.join();
-    //message passing in thread using producer and consumer
-    //mpsc multiple producer ,single consumer
-    let (tx, rx) = mpsc::channel();
-    thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
-    });
-    //rec thread val
-    let received = rx.recv().unwrap();
-    println!("Got: {}", received);
-    let (tx, rx) = mpsc::channel();
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("thread"),
-        ];
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(2));
-        }
-    });
-    for received in rx {
-        println!("Got: {}", received);
-    }
+    // ==================haystack problem=============
+    // let haytsack=String::from("sadsoiamsad");
+    // let needle=String::from("sad");
+    // let res=str_str(&haytsack,&needle);
+    // println!("{:?}",res);
+    // println!("This is {} neat", style("quite").blue());
+    // println!("This is {} neat", style("testing color").red());
+    // let nums=vec![1,3,4,4,3,10];
+    // let target=13;
+    // let rs=two_sum(nums,target);
+    // println!("{:?}",rs);
+    // let version1="1.20";
+    // let version=String::from("1.30");
+    // let version2=String::from("1.30");
+    // let ver_call=compare_version(&version,&version2);
+    // println!("{}",ver_call);
+    // =======================word_dic code============
+    // let mut word_dic=vec!["apple","pen","apple","dog"];
+    // let word=String::from("applepen");
+    // // word_dic.push("apple");
+    // // word_dic.push("pen");
+    // // word_dic.push("apple");
+    // if word_break(&word,word_dic)
+    // { println!("true") }
+    // else { println!("false") }
+    //==============Reverse a string like hello word======>word hello =======
+    // let s=String::from("hello world");
+    // let final_str=reverse_words(s);
+    // println!("{:?}",final_str);
+    // let tmp_str=String::from("aacccbb");
+    // let res=string_compress(tmp_str);
+    // println!("{:?}",res);
+    let test_ana_gram=vec![String::from("ate")
+                           , "eta".parse().unwrap(),
+                           "str".parse().unwrap(),
+                           "tsr".parse().unwrap()];
+    let test_02=vec![String::from("eat")
+                     ,String::from("tea"),
+                     String::from("tan"),
+                     String::from("ate"),
+                     String::from("nat"),
+                     String::from("bat")];
+    group_anagram(&test_02);
 
 }
 fn this_function(test_str:&String)->String
@@ -164,7 +113,7 @@ fn valid_par(test_str:&String)->bool
         if i=='['{ round.push(i); }
         else if i=='(' { bar.push(i); }
         else if i=='{'{curl.push(i);}
-            //reverse match
+        //reverse match
         else if i==']'{
             if round.len()>0{
                 round.pop();
@@ -212,9 +161,9 @@ fn str_str(haystack:&String,needle:&String)->String
     if haystack.contains(needle)
     {
         let result = haystack.find(needle);
-       let index_val=  match result {
-        Some(index) => format!("{}", index),
-        None => format!("'{}' not found", needle), };
+        let index_val=  match result {
+            Some(index) => format!("{}", index),
+            None => format!("'{}' not found", needle), };
         match index_val.parse::<i32>() {
             Ok(n) => println!("The number is: {}", n),
             Err(e) => println!("Failed to parse the string: {}", e),
@@ -229,7 +178,7 @@ fn str_str(haystack:&String,needle:&String)->String
 }
 //Return the index whose sum is = target
 fn two_sum(nums: Vec<i32>, target: i32) -> Vec<usize> {
-let mut index_array=Vec::new();
+    let mut index_array=Vec::new();
     for (i,j) in nums.iter().enumerate(){
         println!("{}{}",i,j);
         // let sum= nums[i]+nums[i+1];
@@ -286,30 +235,71 @@ fn reverse_words(s:String)->String{
     // println!("{:?}",reverse_string_list);s
     final_str
 }
-// fn is_scramble(s1:String,s2:String)->bool
-// {
-//     if s1.len()<1
-//     {
-//         return false
-//     }
-//     for i in 0..s1.len(){
-//
-//     }
-//
-//
-//     true
-// }
-fn permute(array:Vec<i32>)->Vec<i32>
+fn string_compress(S: String) -> String {
+    let mut char_count: HashMap<char, i32> = HashMap::new();
+
+    for c in S.chars() {
+        let counter = char_count.entry(c).or_insert(0);
+        // println!("{:?}",char_count);
+        println!("{}===>{}",c,counter);
+        *counter += 1;
+    }
+
+    let mut compressed = String::new();
+    for (char, count) in char_count {
+        compressed.push(char);
+        compressed.push_str(&count.to_string());
+    }
+
+    compressed
+}
+fn group_anagram(array: &Vec<String>) ->&Vec<String>
 {
-    for i in 0..array.len()
-    {
-        // let tmp_vec=Vec::new();
+    // let hash_vec=HashMap::new();
+    let mut result_vec=Vec::new();
+    for i in 0..array.len(){
+        let mut tmp_vec=Vec::new();
         for j in i..array.len()
         {
-            println!("{:?}",j);
-        }
+            println!("{}{}",i,j);
+            // if i==j{
+            //
+            // }
 
+            let s1=array[i].clone();
+            let s2=array[j].clone();
+            let mut chars: Vec<char> = s1.chars().collect();
+            chars.sort();
+            let sorted_string_1: String = chars.into_iter().collect();
+            let mut chars_02: Vec<char> = s2.chars().collect();
+            chars_02.sort();
+            let sorted_string: String = chars_02.into_iter().collect();
+            println!("{} and two {}",sorted_string,sorted_string_1);
+            if i!=j{
+                if sorted_string_1==sorted_string
+                {
+                    tmp_vec.push(sorted_string);
+                    tmp_vec.push(sorted_string_1);
+                    // result_vec.push(tmp_vec);
+                }
+            }
+            // println!("string one {:?} and string two {:?}",chars,chars_02);
+            // println!(" tmp vec res {:?}",tmp_vec);
+            let  tmp_copy=tmp_vec.clone();
+            if tmp_copy.len()>0
+            {
+                if result_vec.contains(&tmp_copy){
+                    println!("dup");
+                }
+                else {result_vec.push(tmp_copy)};
+            }
+                // if result_vec.len()>0
+            // {
+            //
+            // }
+        }
+        // println!("{}",i);
     }
-array
+    println!("{:?}",result_vec);
+ array
 }
-//threads
